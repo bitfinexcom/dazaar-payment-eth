b3baac3536e562f85373fdf48dadbe1f6ddca212
Christophe Diederichs <chris.d.itunes@gmail.com>
2020-12-15 15:47:40 +0100

    reference payment module fn

--- subscription.js
+++ subscription.js
@@ -49,7 +49,7 @@ module.exports = function configure (index) {
     })
 
     sub.active = payments.active
-    sub.remainingTime = () => payments.remainingTime()
+    sub.remainingTime = payments.remainingTime
     sub.remainingFunds = payments.remainingFunds
 
     sub.destroy = function () {
